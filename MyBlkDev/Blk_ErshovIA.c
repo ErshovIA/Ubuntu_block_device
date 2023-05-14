008940d5e353658669d9aad249adfc5c5fbd59cf
Ilya Ershov <ilyaerhov2000@yandex.ru>
2023-05-14 22:54:54 +0300

    My block device SECOND iteration +IOCTL

--- MyBlkDev/Blk_ErshovIA.c
+++ MyBlkDev/Blk_ErshovIA.c
@@ -50,6 +50,10 @@ static int __init BlockDeviceInit(void)
         DBG_MSG("kzalloc failed!!!!\n");
         return -1;
     }
+    else
+    {
+        DBG_MSG("kzalloc success, DeviceBuffer = %p", DeviceBuffer);
+    }
     DeviceDataLength = 0;
 
     DBG_MSG("Ershov Block drv initialization completed successfully");
@@ -77,12 +81,54 @@ static int BlockDeviceOpen(struct block_device* Device, fmode_t Mode)
 
 static void BlockDeviceRelease(struct gendisk* GenDisk, fmode_t Mode)
 {
-    DBG_MSG("BlockDeviceRelease\n");
+    DBG_MSG("BlockDeviceRelease CALLED\n");
 }
 
 static int BlockDeviceIOCTL(struct block_device* Device, fmode_t Mode, unsigned int Cmd, unsigned long Arg)
 {
     DBG_MSG("BlockDeviceIOCTL CALLED\n");
+
+    block_drv_ioctl_arg* Data = (block_drv_ioctl_arg*)Arg;
+
+    switch(Cmd)
+    {
+        //////// FROM DRIVER TO USER ///////////////
+            case IOCTL_BLK_GET:
+                DBG_MSG("IOCTL_BLK_GET\n");
+                // memset(Data->CopiedToUserData, 0, CopiedToUserLength);
+                int length = DeviceDataLength > Data->CopiedToUserLength ? Data->CopiedToUserLength : DeviceDataLength;
+                copy_to_user(Data->CopiedToUserData, DeviceBuffer, length);
+            break;
+
+        //////// FROM USER TO DRIVER ///////////////
+            case IOCTL_BLK_SET:
+                DBG_MSG("IOCTL_BLK_SET\n");
+                memset(DeviceBuffer, 0, DEVICE_BUFFER_SIZE);
+                int length = DEVICE_BUFFER_SIZE > Data->CopiedFromUserLength ? Data->CopiedFromUserLength : DEVICE_BUFFER_SIZE;
+                copy_from_user(DeviceBuffer, Data->CopiedFromUserData, length);
+                DeviceDataLength = length;
+            break;
+
+        //////// FROM DRIVER TO USER THEN FROM USER TO DRIVER ///////////////
+            case IOCTL_BLK_GET_AND_SET:
+                DBG_MSG("IOCTL_BLK_GET_AND_SET\n");
+                ////////// FROM DRIVER TO USER //////////
+                int length = DeviceDataLength > Data->CopiedToUserLength ? Data->CopiedToUserLength : DeviceDataLength;
+                copy_to_user(Data->CopiedToUserData, DeviceBuffer, length);
+                ////////// FROM USER TO DRIVER //////////
+                memset(DeviceBuffer, 0, DEVICE_BUFFER_SIZE);
+                length = DEVICE_BUFFER_SIZE > Data->CopiedFromUserLength ? Data->CopiedFromUserLength : DEVICE_BUFFER_SIZE;
+                copy_from_user(DeviceBuffer, Data->CopiedFromUserData, length);
+                DeviceDataLength = length;
+            break;
+
+        //////// FROM DRIVER TO USER ///////////////
+            case IOCTL_BLK_DBG_MSG:
+                DBG_MSG("IOCTL_BLK_DBG_MSG:\n");
+                DBG_MSG("%s", DeviceBuffer);
+            break;
+
+    }
 
 
     return 0;
--- MyBlkDev/block_drv_ioctl.h
+++ MyBlkDev/block_drv_ioctl.h
@@ -8,7 +8,14 @@
 #define DRV_GET_AND_SET 0x72
 #define DRV_DBG_MESSAGE 0x73
 
-#define IOCTL_DRV_GET _IOR(DRV_MAGIC, DRV_GET, char*)
-#define IOCTL_DRV_SET _IOW(DRV_MAGIC, DRV_SET, char*) 
-#define IOCTL_DRV_GET_AND_SET _IOWR(DRV_MAGIC, DRV_GET_AND_SET, char*)
-#define IOCTL_DRV_DBG_MESSAGE _IO(DRV_MAGIC, DRV_DBG_MESSAGE) 
+#define IOCTL_BLK_GET _IOR(DRV_MAGIC, DRV_GET, char*)
+#define IOCTL_BLK_SET _IOW(DRV_MAGIC, DRV_SET, char*) 
+#define IOCTL_BLK_GET_AND_SET _IOWR(DRV_MAGIC, DRV_GET_AND_SET, char*)
+#define IOCTL_BLK_DBG_MSG _IO(DRV_MAGIC, DRV_DBG_MESSAGE) 
+
+typedef struct _block_drv_ioctl_arg{
+    char* CopiedToUserData;
+    char* CopiedFromUserData;
+    int CopiedToUserLength; // Length of buffer, NOT a number of readed bytes !!!!
+    int CopiedFromUserLength; // Length of message that we have in CopiedFromUserData Buffer !!!!
+} block_drv_ioctl_arg;
